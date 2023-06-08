9aa7cae251f64475ab00aea4ce2f20e2fd073fbd
RealKingTino <tmowizzy@gmail.com>
2023-06-08 11:58:27 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+
 import sys
 
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
-
-print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
-if num_arguments > 0:
-    for i, argument in enumerate(arguments):
-        position = i + 1
-        print(f"{position}: {argument}")
-else:
-    print(".")
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+    argument = len(arguments)
+    if argument == 0:
+        print('{} arguments.'.format("0"))
+    elif argument == 1:
+        print('{} argument:'.format(argument))
+    else:
+        print('{} arguments:'.format(argument))
+    for i in range(argument):
+        print('{}: {}'.format(i + 1, arguments[i]))
