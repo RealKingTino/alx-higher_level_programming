bf71d10a76bc8f95768d659053cee695287370e0
RealKingTino <tmowizzy@gmail.com>
2023-06-08 05:45:15 -0700

    refactd 2-args to meet required

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import sys
+
 arguments = sys.argv[1:]
 num_arguments = len(arguments)
-print(f"{num_arguments} arguments:")
-for i in range(num_arguments):
-    position = i + 1
-    argument = arguments[i]
-    print(position, ":", argument)
+
+print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
+if num_arguments > 0:
+    for i, argument in enumerate(arguments):
+        position = i + 1
+        print(f"{position}: {argument}")
+else:
+    print(".")
