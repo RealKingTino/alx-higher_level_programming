6cd3cda8553681632ec1f784142a960d6bd69bc8
RealKingTino <tmowizzy@gmail.com>
2023-06-27 13:03:33 -0700

    a class Square that defines a square by: (based on 4-square.py)

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" class Square that defines a square"""
+
+
+class Square:
+    """ class Square that defines a square"""
+    def __init__(self, size=0):
+        """
+        Initializes a Square instance with an optional size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        Attributes:
+            __size (int): The private instance attribute to
+            store the size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the current size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new size value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value  #: size of the square
+
+    def area(self):
+        """
+        Returns the current area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square with '#' characters.
+
+        If size is 0, an empty line is printed.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
