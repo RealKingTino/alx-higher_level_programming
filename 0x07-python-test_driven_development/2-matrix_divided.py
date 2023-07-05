b9c83d6000e47ba1daa489d8f56cd058838ad3f2
RealKingTino <tmowizzy@gmail.com>
2023-07-05 07:15:22 -0700

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,29 +15,38 @@ def matrix_divided(matrix, div):
         div (int or float): Number to divide the matrix elements by.
 
     Returns:
-        list of lists: New matrix with elements divided by div, rounded to 2 decimal places.
+        list of lists: New matrix with elements divided by div,
+        rounded to 2 decimal places.
 
     Raises:
         TypeError: If the matrix is not a list of lists of integers/floats, or if each row of the matrix doesn't have the same size.
         TypeError: If div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
-
+def matrix_divided(matrix, div):
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    
+    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    row_size = len(matrix[0])
+    if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-
+    
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
+    
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
+    
     new_matrix = []
     for row in matrix:
-        new_row = [round(element / div, 2) for element in row]
+        new_row = []
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_row.append(round(element / div, 2))
         new_matrix.append(new_row)
-
+    
     return new_matrix
