f8b8e2cafe6ca1347125c9fc839bb6d962ffcfca
RealKingTino <tmowizzy@gmail.com>
2023-09-17 09:37:48 -0700

    pycode style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Lists states with names starting with 'N' from the database hbtn_0e_0_usa """
+""" Lists states with names starting with
+'N' from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -7,12 +9,13 @@ import sys
 if __name__ == "__main__":
     username, password, database = sys.argv[1:4]
 
-    db = MySQLdb.connect(host="localhost", 
+    db = MySQLdb.connect(host="localhost",
                          user=username, passwd=password,
                          db=database, port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT DISTINCT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cursor.execute("SELECT DISTINCT * FROM states WHERE
+                   name LIKE 'N%' ORDER BY states.id")
 
     for row in cursor.fetchall():
         print(row)
