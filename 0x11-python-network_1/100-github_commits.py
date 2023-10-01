4f2f936d7ba03d944561fa4ae477920957cf70e4
RealKingTino <tmowizzy@gmail.com>
2023-10-01 05:05:10 -0700

    A script that retrieves the latest 10 commits from a GitHub repository

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,12 +8,14 @@ import requests
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        sys.exit("Usage: {} <repository_name> <owner_name>".format(sys.argv[0]))
+        sys.exit("Usage: {} <repository_name> <owner_name>"
+                 .format(sys.argv[0]))
 
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name)
+    url = ("https://api.github.com/repos/{}/{}/commits"
+           .format(owner_name, repo_name))
 
     response = requests.get(url)
     commits = response.json()
