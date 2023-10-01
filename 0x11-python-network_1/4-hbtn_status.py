c409a8a4936c218032ca746b80c21b7ca3770cf5
RealKingTino <tmowizzy@gmail.com>
2023-10-01 04:45:32 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,13 +3,13 @@
 Fetches content from a URL using the requests package.
 """
 
-import requests
 
 if __name__ == "__main__":
+    import requests
     url = "https://alx-intranet.hbtn.io/status"
 
     response = requests.get(url)
 
     print("Body response:")
-    print("\t- type: {}".format(type(response.text).__name__))
+    print("\t- type: {}".format(response.text.__class__))
     print("\t- content: {}".format(response.text))
