984b9ce6ddf001a28e92aa74c0e1d0c15ac208c9
RealKingTino <tmowizzy@gmail.com>
2023-11-02 02:32:09 -0700

    Aliyu - JavaScript Web_jQuery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data){
+    const movies = data.results;
+    const listMovies = $('#list_movies');
+    movies.forEach(function(movie){
+      listMovies.append(`<li>${movie.title}</li>`);
+    });
+  });
+});
